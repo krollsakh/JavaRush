18be0617a04f66eedc7bfe878a6b95e48c60b6d1
Sergey <kroll@mail.ru>
2020-05-30 22:33:04 +0700

    Task 3505

--- 4.JavaCollections/src/com/javarush/task/task35/task3505/ConvertableUtil.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3505/ConvertableUtil.java
@@ -6,8 +6,13 @@ import java.util.Map;
 
 public class ConvertableUtil {
 
-    public static Map convert(List list) {
+    public static Map convert(List<? extends Convertable> list) {
         Map result = new HashMap();
+
+        for (int i = 0; i < list.size(); i++) {
+
+            result.put(list.get(i).getKey(), list.get(i));
+        }
 
         return result;
     }
